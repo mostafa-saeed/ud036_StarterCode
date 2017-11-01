73a1393a7967eec58b267f61e6322e78f5a8b9a6
Mostafa Saeed <mostafa.saeed543@gmail.com>
2017-11-01 14:24:52 +0200

    update gh pages

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(document).on('click', '.hanging-close', function (event) {
 // Start playing the video whenever the trailer modal is opened
 $(document).on('click', 'a[data-trailer-youtube-id]', function (event) {
     var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
-    var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
+    var sourceUrl = 'https://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
     $("#trailer-video-container").empty().append($("<iframe></iframe>", {
         'id': 'trailer-video',
         'type': 'text-html',
