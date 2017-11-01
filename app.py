d6dbebed7c14e7ae4c8752ea05bca2fb1015260a
Mostafa Saeed <mostafa.saeed543@gmail.com>
2017-11-01 13:18:30 +0200

    increase videos

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import media
 from fresh_tomatoes import open_movies_page
 from themoviedb import get_top_rated
 
-TOP_RATED_MOVIES = get_top_rated(1)
+TOP_RATED_MOVIES = get_top_rated(20)
 movies = []
 for top_rated_movie in TOP_RATED_MOVIES:
     movie = media.Movie(top_rated_movie['title'], top_rated_movie['poster_url'], top_rated_movie['video'])
